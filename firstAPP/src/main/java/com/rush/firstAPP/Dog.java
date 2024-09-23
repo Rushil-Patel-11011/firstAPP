ea1f91f4c4e23646bb65d149e31892f1ed8938f8
Rushil Kakadiya <rushilpatel11011@gmail.com>
2024-09-23 16:02:00 +0530

    first commit

---
+++ firstAPP/src/main/java/com/rush/firstAPP/Dog.java
@@ -0,0 +1,10 @@
+package com.rush.firstAPP;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dog {
+    public String fun(){
+        return "Something";
+    }
+}
---
+++ firstAPP/src/main/java/com/rush/firstAPP/MyClass.java
@@ -0,0 +1,14 @@
+package com.rush.firstAPP;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyClass {
+    @Autowired
+    private Dog dog;
+    @GetMapping("hello")
+    public String hello() {return dog.fun();
+    }
+}
